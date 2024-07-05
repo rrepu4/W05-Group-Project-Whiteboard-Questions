2a4e521fff7aa0716c6d2bd4392fe87bf9b68645
Rohit Repu <rrepu4@gmail.com>
2024-07-04 22:55:56 -0500

    steP3

--- javascript.js
+++ javascript.js
@@ -17,12 +17,14 @@ for (let i = original.length - 1; i >= 0; i--) {
 console.log('%cReverse String using for loop: ','font-weight:bold');
 console.log('%cOriginal String is: ','font-weight:bold',original);
 console.log('%cReversed String is: ','font-weight:bold', reversed);
+console.log('\n');
 
 //2. Fizz - Buzz
 let cvar = 15;
 
 console.log(cvar);
 console.log(takeNumber(cvar));
+console.log('\n');
 
 function takeNumber(x){
 //console.log(x);
@@ -31,3 +33,19 @@ else if(x % 3 == 0){return 'Fizz';}
 else if(x % 5 == 0 ){return 'Buzz';}
 else { return x;}
 }
+
+
+//3. Leap Year 
+let varYear = '2024';
+//takeYear(varYear);
+console.log('Year entered is: ', varYear);
+console.log('Is it a leap year?', takeYear(varYear));
+console.log('\n');
+function takeYear(paraYear){
+if (paraYear % 400 == 0){return true;}
+else if (paraYear % 100 == 0){return false;}
+else if (paraYear % 4 == 0){return true;}
+else { return false;}
+}
+
+// 4. BLT
